435cb719e84d7af1eec21d1b476246290a9c8a5b
MGenteluci <mgenteluci97@gmail.com>
2018-08-04 09:18:25 -0300

    Change in CORS Handler
    - Typing error, changed from 'Methos' to 'Methods'

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methos', 'GET, PUT, DELETE, POST, PATCH');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST, PATCH');
         return res.status(200).json({});
     }
 
