344a5aa10388afbb893f4f121c0d83f641f6de2c
MGenteluci <mgenteluci97@gmail.com>
2018-09-11 01:07:18 -0300

    Cors, EasterEgg
    - Added cors dependency
    - Added author's easter egg

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const db = require('./api/conf/database');
 
 const teamRoutes = require('./api/routes/teams');
@@ -17,25 +18,14 @@ if(process.env.NODE_ENV !== 'test')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST, PATCH');
-        return res.status(200).json({});
-    }
-
-    next();
-});
+app.use(cors());
 
 app.use('/teams', teamRoutes);
 app.use('/users', userRoutes);
 app.use('/homeOffices', homeOficeRoutes);
 app.use('/roles', roleRoutes);
 
-app.get('/', (req, res, next) => res.status(204).json());
+app.get('/author', (req, res, next) => res.status(200).json({ author: 'Matheus Genteluci' }));
 
 app.use((req, res, next) => res.status(404).json({ message: 'Not found' }));
 
