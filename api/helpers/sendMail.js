b2881ba42f4b2d099e80a3e67a5937e81ce7d9e8
MGenteluci <mgenteluci97@gmail.com>
2018-09-13 13:42:36 -0300

    Sending team as param for mailSender

--- api/helpers/sendMail.js
+++ api/helpers/sendMail.js
@@ -3,9 +3,9 @@ const mailer = require('../conf/mail');
 exports.sendMail = (req, res, next) => {
     
     let day = new Date(req.day);
-    day = `${day.getDate()+1}/${day.getMonth()+1}/${day.getFullYear()}`;
+    day = `${day.getDate()}/${day.getMonth()+1}/${day.getFullYear()}`;
     
-    mailer.send(req.mailTo, req.userFullName, day);
+    mailer.send(req.mailTo, req.userFullName, day, req.team);
 
     return res.status(201).json({ message: 'Home Office added' });
 };
